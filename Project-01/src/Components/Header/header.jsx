04ef723b2f8ba5f5e6b1302351fe742b1e1067df
Lorrayne Lima Mota <lorraynelimamota@gmail.com>
2024-10-10 12:38:33 -0300

    ADD: Structure Header

--- Project-01/src/Components/Header/header.jsx
+++ Project-01/src/Components/Header/header.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import Logo from './img/Logo CM.png'
 
-function header() {
+export default function Header() {
   return (
-    <>
-    <h1>Cor de Rosa e eu Linkei</h1>
-    </>
+    <header>
+      <figure>
+        <img src={Logo} alt="Logo com uma pessoa com um laço rosa frente ao corpo indicando o outubro rosa, mês de prevenção ao câncer de mama" />
+        <h1>Outubro Rosa</h1>
+      </figure>
+      <nav>
+        <ul>
+          <li>Sobre</li>
+          <li>Conscientização</li>
+          <li>Dicas</li>
+        </ul>
+      </nav>
+    </header>
   )
 }
-
-export default header
