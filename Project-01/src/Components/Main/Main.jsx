42649556d528a5256b76834f8f163022b7148e7d
Lorrayne Lima Mota <lorraynelimamota@gmail.com>
2024-10-10 13:48:51 -0300

    START: style on main and ADD: classNames on figure

--- Project-01/src/Components/Main/Main.jsx
+++ Project-01/src/Components/Main/Main.jsx
@@ -11,19 +11,19 @@ export default function Main() {
     <main className='MainOut'>
     <section>
         <p>Outubro Rosa alerta sobre prevenção e diagnóstico precoce do câncer de mama. O mês de Outubro já é conhecido mundialmente como um mês marcado por ações afirmativas relacionadas à prevenção e diagnóstico precoce do câncer de mama. O movimento, conhecido como Outubro Rosa, é celebrado anualmente desde os anos 90.</p>
-        <figure>
+        <figure className='Fig01'>
             <img src={Hand} alt="Imagem de várias mãos segurando laços rosa em forma de um circulo contendo a seguinte frase no meio 'Outubro Rosa, Mês de conscientização ao cancer de mama'" />
         </figure>
     </section>
     <section>
-        <figure>
+        <figure className='Fig02'>
             <img src={Run}
             alt="Varias mulheres vestidas com a camisa cor de rosa simbolizando as cores da campanha em uma corrida" />
         </figure>
         <p>Outubro Rosa promove a conscientização sobre o câncer de mama com corridas e caminhadas. Esses eventos incentivam a prevenção e o diagnóstico precoce, reunindo pessoas em apoio à causa. Participantes vestem rosa e percorrem distâncias variadas.</p>
     </section>
     <section>
-        <figure>
+        <figure className='Fig03'>
             <img src={Mamografia} alt="" />
             <img src={Alimentacao} alt="" />
             <img src={Medico} alt="" />
