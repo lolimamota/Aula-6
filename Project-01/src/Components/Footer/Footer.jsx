3019cf8dd4b73aa1eb847cf96e7bb26a2caa1107
Lorrayne Lima Mota <lorraynelimamota@gmail.com>
2024-10-11 12:22:05 -0300

    FINISH: Aligns on footer

--- Project-01/src/Components/Footer/Footer.jsx
+++ Project-01/src/Components/Footer/Footer.jsx
@@ -7,17 +7,19 @@ import Laco from './img/laço.png';
 
 export default function Footer() {
   return (
-    <footer>
+    <footer className='FooterOut'>
         <figure>
             <img src={LinkedIn} alt="logo do Linkedin" />
             <img src={Instagram} alt="logo do Instagram" />
             <img src={Github} alt="logo do Github" />
         </figure>
+        <div>
         <h3>Lorrayne Lima Mota</h3>
         <h3>Vinicius Bispo</h3>
         <h3>Marina Gomes</h3>
         <h3>Layout criado para fins de estudos na escola vai na web</h3>
-        <figure>
+        </div>
+        <figure className='FigEnd'>
             <img src={Laco} alt="Laço de fita rosa representando o mês da prevenção ao câncer de mama" />
         </figure>
     </footer>
