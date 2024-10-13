f242c69e45142cdd13d8ca885af4b6c85a07b652
Lorrayne Lima Mota <lorraynelimamota@gmail.com>
2024-10-13 11:09:42 -0300

    RM: import de rotas

--- Project-01/src/App.jsx
+++ Project-01/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import Header from './Components/Header/header.jsx';
 import Main from './Components/Main/Main.jsx';
 import Footer from './Components/Footer/Footer.jsx';
-import Router from './Components/Routes/Route.jsx';
+
 
 
 export default function App() {
