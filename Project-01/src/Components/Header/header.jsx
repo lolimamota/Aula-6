e5e91bfc1e597f9c3f2fd02e0c916c87c1c2cfbb
Lorrayne Lima Mota <lorraynelimamota@gmail.com>
2024-10-12 22:54:10 -0300

    Complete Anchorage

--- Project-01/src/Components/Header/header.jsx
+++ Project-01/src/Components/Header/header.jsx
@@ -1,18 +1,34 @@
 import React from 'react'
 import Logo from './img/Logo CM.png'
 
+import SobreMain from '../Routes/SobreMain.jsx';
+import ConsienteMain from '../Routes/ConscienteMain.jsx';
+import DicasMain from '../Routes/DicasMain.jsx';
+
 export default function Header() {
   return (
     <header className='HeaderOut'>
-      <figure>
+        <figure>
         <img src={Logo} alt="Logo com uma pessoa com um laço rosa frente ao corpo indicando o outubro rosa, mês de prevenção ao câncer de mama" />
         <h1>Outubro Rosa</h1>
-      </figure>
+        </figure>
       <nav>
         <ul>
-          <li>Sobre</li>
-          <li>Conscientização</li>
-          <li>Dicas</li>
+          <li>
+            <a href='../Routes/SobreMain.jsx' target='_blank'>
+            Sobre
+            </a>
+          </li>
+          <li>
+            <a href='../Routes/ConscienteMain.jsx' target='_blank'>
+            Conscientização
+            </a>
+          </li>
+          <li>
+            <a href='../Routes/DicasMain.jsx' target='_blank'>
+            Dicas
+            </a>
+          </li>
         </ul>
       </nav>
     </header>
